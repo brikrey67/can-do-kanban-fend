ffc62f3c5109c97b38e18144f7f664840831042d
Brian Reynolds <brikrey@gmail.com>
2018-01-23 12:56:54 -0500

    converted static get to axios get pulling from heroku

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,55 +7,66 @@ import Login from "../Login/Login.js";
 import Buckets from "../Buckets/Buckets.js";
 
 import { Route, Link, Redirect, Switch } from "react-router-dom";
+import axios from "axios";
 import "./App.css";
 
-const buckets = [
-  {
-    bOrder: 1,
-    bTitle: "backlog",
-    bDesc: "bucket of tasks that might get executed",
-    intCrit:
-      "tasks or ideas seems to have business value, but hasn't been validated",
-    exCrit: "tasks or ideas have been validated as valuable and affordable"
-  },
-  {
-    bOrder: 2,
-    bTitle: "to-do",
-    bDesc: "bucket of tasks that are planned for execution",
-    intCrit: "tasks or ideas have been validated as valuable and affordable",
-    exCrit: "tasks or ideas that will be taken on next"
-  },
-  {
-    bOrder: 3,
-    bTitle: "in-progress",
-    bDesc: "bucket of tasks that are being activily worked on",
-    intCrit: "tasks or ideas that will be taken on next",
-    exCrit: "tasks or ideas have been completed"
-  },
-  {
-    bOrder: 4,
-    bTitle: "completed",
-    bDesc: "bucket of tasks that have been completed",
-    intCrit: "tasks or ideas that have no remaining work",
-    exCrit: "tasks or ideas can be archived"
-  }
-];
+// const buckets = [
+//   {
+//     bOrder: 1,
+//     bTitle: "backlog",
+//     bDesc: "bucket of tasks that might get executed",
+//     intCrit:
+//       "tasks or ideas seems to have business value, but hasn't been validated",
+//     exCrit: "tasks or ideas have been validated as valuable and affordable"
+//   },
+//   {
+//     bOrder: 2,
+//     bTitle: "to-do",
+//     bDesc: "bucket of tasks that are planned for execution",
+//     intCrit: "tasks or ideas have been validated as valuable and affordable",
+//     exCrit: "tasks or ideas that will be taken on next"
+//   },
+//   {
+//     bOrder: 3,
+//     bTitle: "in-progress",
+//     bDesc: "bucket of tasks that are being activily worked on",
+//     intCrit: "tasks or ideas that will be taken on next",
+//     exCrit: "tasks or ideas have been completed"
+//   },
+//   {
+//     bOrder: 4,
+//     bTitle: "completed",
+//     bDesc: "bucket of tasks that have been completed",
+//     intCrit: "tasks or ideas that have no remaining work",
+//     exCrit: "tasks or ideas can be archived"
+//   }
+// ];
 
 class App extends Component {
   constructor(props) {
     super(props);
     // initialize state
     this.state = {
-      buckets,
+      buckets: [],
       targetBucket: null
     };
   }
 
-  // function to pass to Bucket.js
   setBucket(data) {
     this.setState({
       targetStock: data
     });
+  }
+
+  // from react-router lab
+  componentDidMount() {
+    axios
+      .get("https://can-do-kanban-bend.herokuapp.com/bucket")
+      .then(response => {
+        this.setState({
+          buckets: response.data
+        });
+      });
   }
 
   render() {
@@ -77,7 +88,7 @@ class App extends Component {
                 <Buckets
                   {...props}
                   setBucket={data => this.setBucket(data)}
-                  buckets={buckets}
+                  buckets={this.state.buckets}
                 />
               )}
             />
